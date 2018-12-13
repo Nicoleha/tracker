27d07cd4e2b5a5a5257f505263885d2cfcbdfce7
Nicole Hategekimana <nicoleha127@gmail.com>
2018-12-13 10:22:19 +0200

    link to javascript and css done

--- scripts.js
+++ scripts.js
@@ -1,12 +1,13 @@
-function Track(){
-    var firstvalue=document.getElementById('one').nodeValue;
-    var secondvalue=document.getElementById('two').nodeValue;
-    var thirdvalue=document.getElementById('three').nodeValue;
-
-    if(one===two===three){
-        alert("isosceles")
+function track(){
+    var firstvalue=document.getElementById('one').value;
+    var secondvalue=document.getElementById('two').value;
+    var thirdvalue=document.getElementById('three').value;
+    var text;
+    if(firstvalue === secondvalue && secondvalue === thirdvalue){
+        text = "Equilaterial Triangle"
     }
     else{
-        alert("not isosceles")
+        text = "not isosceles"
     }
+    document.getElementById('demo').innerHTML = text;
 }
