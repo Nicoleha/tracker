51fb0c0bfc5ea345147d76db0400d2a45a9a4892
Nicole Hategekimana <nicoleha127@gmail.com>
2018-12-13 09:38:51 +0200

    color added and saved

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function (){
+    var firstvalue=document.getElementById('one').nodeValue;
+    var secondvalue=document.getElementById('two').nodeValue;
+    var thirdvalue=document.getElementById('three').nodeValue;
+
+    if(one===two===three){
+        alert("isosceles")
+    }
+    else{
+        alert("not isosceles")
+    }
+}
